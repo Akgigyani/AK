2be80bfceae5d457ffc94a68cc6b73820a56bed7
Aamir Khan <aamirkgigyani@gmail.com>
2022-10-31 03:55:48 +0000

    layout change for large screen

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,4 @@
-import Loader from 'react-loaders';
+import Loader from 'react-loaders'; 
 import React, { useState } from 'react';
 import './index.scss';
 
@@ -21,12 +21,47 @@ const Contact = () => {
             <span className={`${letterClass} _19`}> </span>
             <span className={`${letterClass} _20`}>M</span>
             <span className={`${letterClass} _21`}>e</span>
-          </h1>
-          <p>
+            </h1>
+          {/* <p>
             I am interested in freelance opportunities - especially on ambitious
             or large projects. However, if you have any other requests or
             questions, don't hesitate to contact me using below form either.
-          </p>
+          </p> */}
+          <div className="contact-form">
+            <form>
+              <ul>
+                <li className="half">
+                  <input placeholder="Name" type="text" name="name" required />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
         </div>
       </div>
       <Loader type="pacman" />
